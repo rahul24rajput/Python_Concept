558be5fcce02d0548b2bfee12fa7a7a936167051
Kanuj Bhatnagar <kanuj.bhatnagar@achieve3000.com>
2020-03-02 01:03:15 +0530

    Using cleaned_data dict for for capturing the data entered by end uder

--- python/firstProject/formProject/views.py
+++ python/firstProject/formProject/views.py
@@ -9,5 +9,17 @@ def studentRegister(request):
 
 def FeedBackView(request):
     feedBack = forms.FeedBackForms()
+    if request.method == 'POST':
+        form = forms.FeedBackForms(request.POST)
+        if form.is_valid():
+            print('From validation success')
+            # How to capture the data enter by end user
+            # So for that we have to use cleaned_data ## type is dictaniory 
+            # {'name': Data entered,} Key will be the same you have defined in forms.py and value will be
+            # the data has been entered by user
+            print('Student Name', form.cleaned_data['name'])
+            print('Student Name', form.cleaned_data['rollno'])
+            print('Student Name', form.cleaned_data['email'])
+            print('Student Name', form.cleaned_data['feedback'])
     return render(request,'temProject/feedback.html',{'form':feedBack}) ### Here this form is mandatory if you are not defining context_dict
                                                                         # key should be form mainly    
