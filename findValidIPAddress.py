6ef6a9a134df7c389f5c5f5b6a1406a63a621599
Rahul Rajput <60896475+rahul24rajput@users.noreply.github.com>
2024-04-28 22:23:17 +0530

    Create findValidIPAddress.py

---
+++ findValidIPAddress.py
@@ -0,0 +1,53 @@
+import re
+
+# 4 numbers seprated by .
+# [0-99].[100-199].[200-249].[250-255]
+
+# [0-9][0-9]?
+# 1[0-9][0-9]
+# 2[0-4][0-9]
+# 25[0-5]
+
+
+
+
+# ^(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[0-9][0-9]?)\.
+# (25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[0-9][0-9]?)\.
+# (25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[0-9][0-9]?)\.
+# (25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[0-9][0-9]?)$
+
+
+
+# importing the module 
+import re 
+
+# opening and reading the file 
+with open('dummy.txt') as fh:
+    string = fh.readlines() 
+	
+# declaring the regex pattern for IP addresses 
+pattern =re.compile('^(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[0-9][0-9]?)$')
+
+# initializing the list objects 
+valid =[] 
+invalid=[] 
+
+# extracting the IP addresses 
+for line in string:
+    line = line.rstrip()
+
+    result = pattern.search(line) 
+	# valid IP addresses 
+    if result:
+        valid.append(line)
+    else:
+        invalid.append(line) 
+
+# displaying the IP addresses 
+print("Valid IPs") 
+print(valid) 
+print("Invalid IPs") 
+print(invalid) 
+
+
+
